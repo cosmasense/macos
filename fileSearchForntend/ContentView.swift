3e055b8f0d8b550851eb30ad3f9ad0ff2156befd
EthanPany <pyy122759996@gmail.com>
2025-10-19 16:43:21 -0500

    Initial Commit

---
+++ fileSearchForntend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fileSearchForntend
+//
+//  Created by Ethan Pan on 10/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fileSearchForntend/fileSearchForntendApp.swift
@@ -0,0 +1,17 @@
+//
+//  fileSearchForntendApp.swift
+//  fileSearchForntend
+//
+//  Created by Ethan Pan on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct fileSearchForntendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
